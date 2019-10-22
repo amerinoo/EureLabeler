51ede38bdcb851757b0443f023033b0aaf6468df
albertmerino <albert.merino@eurecat.org>
2019-10-22 10:10:10 +0200

    Added electron-reload

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow } = require('electron')
+require('electron-reload')(__dirname);
 const path = require('path')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 800,
